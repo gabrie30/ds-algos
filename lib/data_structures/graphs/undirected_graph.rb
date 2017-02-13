a2e9cef861398ec2da69bc564fb7d4005c9cf4e3
Jay Gabriels <jgabriels30@gmail.com>
2017-02-12 22:19:29 -0800

    add specs to undirected graph

--- lib/data_structures/graphs/undirected_graph.rb
+++ lib/data_structures/graphs/undirected_graph.rb
@@ -12,8 +12,8 @@ class Graph
 
   def initialize()
     @vertices = {}
-    @totalVertices = 0
-    @totalEdges = 0
+    @total_vertices = 0
+    @total_edges = 0
   end
 
   def add_vertex(int)
@@ -22,57 +22,66 @@ class Graph
     else
       new_vertex = Node.new(int)
       @vertices[int] = new_vertex
-      @totalVertices += 1
+      @total_vertices += 1
     end
   end
 
-  def getVertex(id)
+  def get_vertex(id)
     @vertices[id] ? @vertices[id] : nil
   end
 
-  def removeVertex(id)
+  def remove_vertex(id)
     if @vertices[id]
-      vertex = @vertices[id] 
+      vertex = @vertices[id]
       vertex.edges.each do |edge_id|
         @vertices[edge_id].edges.delete(id)
       end
       @vertices.delete(id)
-      @totalVertices -= 1
+      @total_vertices -= 1
     else
       return nil
     end
   end
 
-  def addEdge(id1, id2)                   
+  def add_edge(id1, id2)
     if @vertices[id1] && @vertices[id2]
       @vertices[id1].edges[id2] = id2
-      @vertices[id2].edges[id1] = id1 
-      @totalEdges += 1
+      @vertices[id2].edges[id1] = id1
+      @total_edges += 1
     else
       return nil
     end
   end
 
-  def removeEdge(id1, id2)
+  def remove_edge(id1, id2)
     if @vertices[id1] && @vertices[id2]
       if @vertices[id1].edges.has_key?(id2) && @vertices[id2].edges.has_key?(id1)
         @vertices[id1].edges.delete(id2)
         @vertices[id2].edges.delete(id1)
-        @totalEdges -= 1
+        @total_edges -= 1
       else
-        return nil 
+        return nil
       end
     else
-      return nil 
+      return nil
     end
   end
 
-  def findNeighbors(id)
+  def find_neighbors(id)
     if @vertices[id]
       return @vertices[id].edges
     else
       return nil
-    end 
+    end
   end
 end
+
+g = Graph.new
+g.add_vertex(1)
+g.add_vertex(2)
+g.add_vertex(3)
+g.add_edge(1,2)
+g.add_edge(1,3)
+p g
+
 
--- spec/data_structures/graphs/undirected_graph_spec.rb
+++ spec/data_structures/graphs/undirected_graph_spec.rb
@@ -24,8 +24,56 @@ describe Graph do
     before :all do
       @graph = Graph.new
     end
+
     it "should respond to vertices" do
       expect(@graph).to respond_to(:vertices)
+    end
+
+    it "should respond to total vertices" do
+      expect(@graph).to respond_to(:total_vertices)
+    end
+  end
+
+  context "verticies" do
+    before :each do
+      @graph = Graph.new
+    end
+
+    it "should be able to add vertices" do
+      expect(@graph.total_vertices).to eq(0)
+      @graph.add_vertex(1)
+      expect(@graph.total_vertices).to eq(1)
+    end
+
+    it "should be able to remove vertices" do
+      @graph.add_vertex(10)
+      expect(@graph.total_vertices).to eq(1)
+      @graph.remove_vertex(10)
+      expect(@graph.total_vertices).to eq(0)
+    end
+  end
+
+  context "edges" do
+    before :each do
+      @graph = Graph.new
+    end
+
+    it "should be able to add edges" do
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+      expect(@graph.total_edges).to eq(0)
+      @graph.add_edge(1,2)
+      expect(@graph.total_edges).to eq(1)
+    end
+
+    it "should be able to remove edges" do
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+      expect(@graph.total_edges).to eq(0)
+      @graph.add_edge(1,2)
+      expect(@graph.total_edges).to eq(1)
+      @graph.remove_edge(1,2)
+      expect(@graph.total_edges).to eq(0)
     end
   end
 end
